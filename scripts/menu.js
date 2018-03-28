5b03185b632c8b7e84fe5f44311479008c107a39
fantaandcode <domophillius@gmail.com>
2018-03-27 23:59:07 -0400

    Changed colors and added some JS tests

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+function toggleTesting() {
+	var x = document.getElementById("testing");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
